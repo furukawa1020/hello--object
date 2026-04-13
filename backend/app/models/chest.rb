7b04c143d8474eaf032687f5b7b37823a652342b
furukawa1020 <f.kotaro.0530@gmail.com>
2026-04-13 11:42:22 +0900

    feat: add chest model to backend application

--- backend/app/models/chest.rb
+++ backend/app/models/chest.rb
@@ -7,6 +7,28 @@ class Chest < GameObject
     @locked = true
   end
 
+  def ui_actions
+    a = WorldManager::ALIASES.key(@id) || @id
+    [
+      { label: '🗝 鍵を使って開ける', code: "#{a}.unlock(key)" },
+      { label: '📦 中を覗く',        code: "#{a}.open" },
+    ]
+  end
+
+  def ui_schematic
+    <<~RUBY
+      class Chest < GameObject
+        include Container
+
+        def unlock(key_obj)
+          if key_obj.is_a?(Key)
+            @locked = false
+          end
+        end
+      end
+    RUBY
+  end
+
   def unlock(key_obj = nil)
     if key_obj.is_a?(Key)
       @locked = false
