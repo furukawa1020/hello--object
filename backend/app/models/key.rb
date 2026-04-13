659079cd3c2cec55d2c5cc43e112057dd468b543
furukawa1020 <f.kotaro.0530@gmail.com>
2026-04-13 11:55:07 +0900

    feat: add Key model with sprite rendering and completion logic

--- backend/app/models/key.rb
+++ backend/app/models/key.rb
@@ -1,4 +1,16 @@
 class Key < GameObject
+  def ui_sprite
+    "<div class='key-sprite'>
+      <div class='key-loop'></div>
+      <div class='key-shaft'></div>
+      <div class='key-teeth'></div>
+    </div>"
+  end
+
+  def completed?
+    false
+  end
+
   def initialize(id:, name:, description:)
     super
   end
