b6185ea09762ac9ea83ba7fc85045cc7be4db538
furukawa1020 <f.kotaro.0530@gmail.com>
2026-04-13 11:39:31 +0900

    feat: implement GameObject base model and AchievementManager engine service

--- backend/app/models/game_object.rb
+++ backend/app/models/game_object.rb
@@ -20,6 +20,36 @@ class GameObject
 
   # Returns a hash of instance variables for the frontend
   def state
+    {
+      id: @id,
+      name: @name,
+      description: @description,
+      class_name: self.class.name,
+      variables: variable_state,
+      notes: @notes,
+      actions: ui_actions,
+      schematic: ui_schematic
+    }
+  end
+
+  def register!
+    Engine::EventRecorder.world.add_object(self)
+    "#{self.class.name} '#{@name}' が具現化されました。"
+  end
+
+  # Default actions - can be overridden in subclasses
+  def ui_actions
+    []
+  end
+
+  # Default schematic - can be overridden
+  def ui_schematic
+    nil
+  end
+
+  private
+
+  def variable_state
     vars = {}
     instance_variables.each do |var|
       # Skip standard internals and parent references to avoid cycles
@@ -33,19 +63,11 @@ class GameObject
         vars[var.to_s.delete('@')] = val
       end
     end
-    
-    {
-      id: @id,
-      name: @name,
-      description: @description,
-      class_name: self.class.name,
-      variables: vars,
-      notes: @notes
-    }
+    vars
   end
 
-  # This is the "Conversation" hook.
   def talk(method_name, *args)
+    # ...
     if respond_to?(method_name)
       send(method_name, *args)
     else
